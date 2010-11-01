24723a622402fc924a82edf8fda68396457a83a1
Karl Diao <klandor@gmail.com>
2010-11-01 13:30:05 +0800

    exp_SVC.cpp: output to file, all result.

--- exp_SVC.cpp
+++ exp_SVC.cpp
@@ -10,15 +10,16 @@
 
 
 #include <iostream>
+#include <fstream>
 #include "LTCode.h"
 #include "ConvoCode.h"
 #include "randomc.h"
 #include <ctime>
 #include <omp.h>
 
-#define K 60000
+#define K (60000-20)
 #define LT_K 1000
-#define RUN 1
+#define RUN 1000
 using namespace std;
 using namespace CodeSim;
 
@@ -26,15 +27,31 @@ int main(){
 	int Degree_of_Edge[10] = {1, 2, 3, 4, 5, 7, 9, 19, 59, 179};
 	double Omega[10] = {7.9379E-02, 4.0129E-01, 1.0121E-01, 2.1679E-01, 5.0996E-02, 
 		5.8338E-05, 3.9740E-02, 7.7470E-02, 2.1520E-02, 1.1547E-02};
+	string s;
+	cout << "Enter output filename: ";
+	getline(cin, s, '\n');
 	
+	ofstream fout(s.c_str());
+	if (fout.fail()) {
+		cerr << "file: " << s << ": open error.\n";
+		exit(1);
+	}
 	
+	cout << "Enter comment: ";
+	getline(cin, s, '\n');
+	fout << "Comment: " << s << endl;
+	
+	cout << "Enter degree distribution: ";
+	for (int i=0; i<10; i++) {
+		cin >> Omega[i];
+	}
 	CRandomMersenne random(time(0));
 	ConvoCode convo("convo-4-6-10.txt");
-	Permutator<Bit> permut("Interleaver.txt", true);
+	Permutator<Bit> permut("result_s1_1000_s2_25.txt", true);
 	
-	long ber[21][3];
-	for (int i=0; i<21*3; i++) {
-		ber[0][i] = 0;
+	long ber[RUN][21][3];
+	for (int i=0; i<21*3*RUN; i++) {
+		ber[0][0][i] = 0;
 	}
 	
 	#pragma omp parallel for num_threads(6)
@@ -71,19 +88,25 @@ int main(){
 			for (int i=0; i<out.size(); i++) {
 				if ( !(out[i]==m[i]) ) {
 					#pragma omp atomic
-					ber[e][i%3]++;
+					ber[run][e][i%3]++;
 				}
 			}
 		}
 		
 	}
 	
-	cout << "BER:\n";
+	fout << "BER:\n";
 	for (int i=0; i<3; i++) {
-		for (int j=0; j<21; j++) {
-			cout << ber[j][i] / (K/3.0*RUN) << ' ';
+		fout << "Layer " << i << ":\n";
+		for (int run=0; run<RUN; run++) {
+			for (int j=0; j<21; j++) {
+				fout << ber[run][j][i] / (K/3.0*RUN) << ' ';
+			}
+			fout << '\n';
+			
 		}
-		cout << '\n';
+		
+		fout << "\n\n";
 	}
 	//<< ber[0] / (double) (K*RUN/3) << ' '<< ber[1] / (double) (K*RUN/3) << ' '<< ber[2] / (double) (K*RUN/3) << '\n';
 	
