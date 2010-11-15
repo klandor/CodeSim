21ea3b5454bfd569e2963b19538c46646d11752e
Karl Diao <klandor@gmail.com>
2010-11-15 13:37:53 +0800

    decoding_pattern_test.cpp: output raw data to stdout.

--- decoding_pattern_test.cpp
+++ decoding_pattern_test.cpp
@@ -24,51 +24,53 @@ using namespace CodeSim;
 #define K 1000
 #define LEN 100
 #define EPS 0.2
-#define RUN 100000
-#define STEP_SIZE 0.03
+#define RUN 10000
+#define STEP_SIZE 0.05
 #define STEPS 6
-//#define WINDOW_SIZE 10
+//#define MAX_WINDOW_SIZE 100
 
 int tags = 10, windowSize;
 
 int		Degree[10] = {1, 2, 3, 4, 5, 7, 9, 19, 59, 179};//{1,2,3,4,5,8,9,19,65,66};
 
-double  Omega[10] = {7.9379E-02, 4.0129E-01, 1.0121E-01, 2.1679E-01, 5.0996E-02, 
-	5.8338E-05, 3.9740E-02, 7.7470E-02, 2.1520E-02, 1.1547E-02};
+double  Omega[10] ;//= {7.9379E-02, 4.0129E-01, 1.0121E-01, 2.1679E-01, 5.0996E-02, 
+//	5.8338E-05, 3.9740E-02, 7.7470E-02, 2.1520E-02, 1.1547E-02};
+int *l0a[RUN][STEPS]; //, l0a_max[STEPS][1000];
 
 int main(){
 	string s;
-	cout << "Enter output filename: ";
-	getline(cin, s, '\n');
-	ofstream fout(s.c_str());
+//	cerr << "Enter output filename: ";
+//	getline(cin, s, '\n');
+//	ofstream fout(s.c_str());
+//	
+//	cerr << "Enter comment: ";
+//	getline(cin, s, '\n');
+//	fout << "Comment: " << s << endl;
 	
-	cout << "Enter comment: ";
-	getline(cin, s, '\n');
-	fout << "Comment: " << s << endl;
-	
-	cout << "Enter Window Size(>0): ";
+	cerr << "Enter Window Size(>0): ";
 	cin >> windowSize;
 	
-	cout << "Enter degree distribution: ";
+	cerr << "Enter degree distribution: ";
 	for (int i=0; i<10; i++) {
 		cin >> Omega[i];
 	}
 	CRandomMersenne r(time(0));
-	Permutator<Bit> per("Interleaver.txt", true);
-	int l0a[STEPS][1000], l0a_max[STEPS][1000];
-	for (int i=0; i<1000*STEPS; i++) {
-		l0a[0][i]=0;
-		l0a_max[0][i]=0;
-	}
+//	Permutator<Bit> per("Interleaver.txt", true);
+	
+//	for (int i=0; i<1000*STEPS; i++) {
+//		l0a[0][i]=0;
+//		l0a_max[0][i]=0;
+//	}
 	
 	#pragma omp parallel for num_threads(6)
 	for (int run=0; run<RUN; run++) {
 		LT_sim<Bit> lt(K, K*(1+STEPS*STEP_SIZE), tags, Degree, Omega, r.BRandom());
 		
 		for (int s=0; s<STEPS; s++) {
-			int l0h[1000];
-			for (int i=0; i<1000; i++) {
-				l0h[i]=0;
+//			int l0h[1000];
+			l0a[run][s] = new int[windowSize+1];
+			for (int i=0; i<windowSize+1; i++) {
+				l0a[run][s][i]=0;
 			}
 			
 			lt.seqReceive(K*(1+s*STEP_SIZE)-1);
@@ -81,9 +83,9 @@ int main(){
 				}
 			}
 			
-			#pragma omp atomic
-			l0a[s][errNO]++;
-			l0h[errNO]++;
+			//#pragma omp atomic
+			l0a[run][s][errNO]++;
+//			l0h[errNO]++;
 			for (int p=windowSize; p< a.size(); p++) {
 				if (a[p].isErased()) {
 					errNO++;
@@ -93,18 +95,18 @@ int main(){
 				}
 				
 				
-				#pragma omp atomic
-				l0a[s][errNO]++;
-				l0h[errNO]++;
+				//#pragma omp atomic
+				l0a[run][s][errNO]++;
+//				l0h[errNO]++;
 				//cout << errNO << '\n';
 			}
 	
-			for (int i =0; i<windowSize+1; i++) {
-				#pragma omp critical
-				if (l0h[i] > l0a_max[s][i]) {
-					l0a_max[s][i] = l0h[i];
-				}
-			}
+//			for (int i =0; i<windowSize+1; i++) {
+//				#pragma omp critical
+//				if (l0h[i] > l0a_max[s][i]) {
+//					l0a_max[s][i] = l0h[i];
+//				}
+//			}
 		}
 		
 
@@ -113,21 +115,26 @@ int main(){
 		
 		
 	}
-	fout.precision(10);
+	//cout.precision(10);
 	//cout << "BER: "<<ber/(double)(K*RUN)<< endl;
-	fout << "Average:\n";
+//	fout << "Average:\n";
 	for (int s=0; s<STEPS; s++) {
-		for (int i=0; i<windowSize+1; i++) {
-			fout << l0a[s][i] / (double)(RUN*(K-windowSize+1)) << ' ' ;//<< l0a_max[i] << '\n';
+		for (int run = 0; run<RUN; run++) {
+			
+			for (int i=0; i<windowSize+1; i++) {
+				cout << l0a[run][s][i] / (double)((K-windowSize+1)) << '\t' ;//<< l0a_max[i] << '\n';
+			}
+			cout << '\n';
 		}
-		fout << '\n';
+		
+		cout << "\n\n\n\n\n\n\n\n\n\n";
 	}
-	fout << "\nMaximum:\n";
-	for (int s=0; s<STEPS; s++) {
-		for (int i=0; i<windowSize+1; i++) {
-			fout << l0a_max[s][i] / (double)(K-windowSize+1)<< ' ' ;//<< l0a_max[i] << '\n';
-		}
-		fout << '\n';
-	}
+//	fout << "\nMaximum:\n";
+//	for (int s=0; s<STEPS; s++) {
+//		for (int i=0; i<windowSize+1; i++) {
+//			fout << l0a_max[s][i] / (double)(K-windowSize+1)<< ' ' ;//<< l0a_max[i] << '\n';
+//		}
+//		fout << '\n';
+//	}
 	
 }
