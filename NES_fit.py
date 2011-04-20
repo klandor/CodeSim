dbfdf7de24c78b5019ec562d7deb7739041bf128
Karl DIao <klandor@gmail.com>
2011-04-20 17:56:32 +0800

    NES_fit.py: Complete version 1

--- NES_fit.py
+++ NES_fit.py
@@ -3,10 +3,11 @@ from scipy import array
 import math
 import pybrain
 from pybrain.optimization import ExactNES
-
-# if(len(sys.argv) == 1):
-# 	print "Usage: NES_fit.py filename"
-# 	sys.exit()
+import os
+os.nice(20)
+if(len(sys.argv) == 1):
+	print "Usage: NES_fit.py filename"
+	sys.exit()
 
 def parseInt(a):
 	b=[]
@@ -53,6 +54,7 @@ def abs(x):
 
 class LT_exp:
 	def __init__(self, filename):
+		#read from config file
 		f=open(filename,'r')
 		self.K = int(mygetline(f))
 		self.Dsize = int(mygetline(f))
@@ -73,31 +75,41 @@ class LT_exp:
 		self.epsilons_w = parseFloat(mygetline(f).split())
 		self.targetErrorRate = parseFloat(mygetline(f).split())
 		self.targetErrorRate_w = parseFloat(mygetline(f).split())
-
-
-	def fitness_LT(self, dist):
-		LT_BER_format = "{K} {Run}\n{Dsize}\n{tag_list}\n{distribution}\n{STEPS}\n{epsilon_list}"
-		STEPS = 61
-		Delta = 0.005
-		Run = 10000
-		MaxEpsilon = (Delta*(STEPS-1))
-		P_e_min = log10(1/float(self.K*Run))
 		
-		epsilon_list = array(parseFloat(range(STEPS)))*Delta
+		# prepare LT_BER process
+		LT_BER_format = "{K} {Run}\n{Dsize}\n{tag_list}\n{STEPS}\n{epsilon_list}\n"
+		self.STEPS = 61
+		self.Delta = 0.005
+		self.Run = 10000
+		self.MaxEpsilon = (self.Delta*(self.STEPS-1))
+		self.P_e_min = log10(1/float(self.K*self.Run))
+		
+		self.epsilon_list = array(parseFloat(range(self.STEPS)))*self.Delta
 		str_epsilon_list = []
-		for i in range(STEPS):
-			str_epsilon_list.append('%.3f' % epsilon_list[i])
-		
-		d=dist[0:]
-		normalize(d)
+		for i in range(self.STEPS):
+			str_epsilon_list.append('%.3f' % self.epsilon_list[i])
 		from subprocess import Popen
 		from subprocess import PIPE
 		
-		p = Popen('./LT_BER.out', stdin=PIPE, stdout=PIPE)
-		input = LT_BER_format.format(K=self.K, Run=Run, Dsize=self.Dsize, tag_list=' '.join(toStringList(self.Tags)), distribution=' '.join(toStringList(d)), STEPS=STEPS, epsilon_list= ' '.join(str_epsilon_list))
+		self.p = Popen('../../LT_BER.out', stdin=PIPE, stdout=PIPE)
+		input = LT_BER_format.format(K=self.K, Run=self.Run, Dsize=self.Dsize, tag_list=' '.join(toStringList(self.Tags)), STEPS=self.STEPS, epsilon_list= ' '.join(str_epsilon_list))
+		self.p.stdin.write(input)
+		
+		#prepare listener for ExactNES to write out results
+		self.file_result = open('result_%s' % filename, 'w')
+		self.result_line = 0
+		self.times_of_eval = 0
+	def fitness_LT(self, dist):
+		self.times_of_eval += 1
+		
+		d=dist[0:]
+		normalize(d)
+		input=' '.join(toStringList(d))
 		#K='`self.K`', Run='100', Dsize='`self.Dsize`', tag_list=' '.join(self.Tags), distribution='`self.D`', STEPS='16', epsilon_list= "")
 		#print input
-		err =  p.communicate(input)[0]
+		self.p.stdin.write('%s\n' % input)
+		#err =  p.communicate(input)[0]
+		err = self.p.stdout.readline()
 		err = err.split()
 		err = parseFloat(err)
 		for i in range(len(err)):
@@ -110,28 +122,53 @@ class LT_exp:
 		fit = 0
 		
 		for i in range(len(self.epsilons)) :
-			fit += (err[int(self.epsilons[i]/Delta)] - P_e_min)  / abs( P_e_min ) * self.epsilons_w[i];
+			fit += (err[int(self.epsilons[i]/self.Delta)] - self.P_e_min)  / abs( self.P_e_min ) * self.epsilons_w[i];
 			#parameters[i] = (err[int(self.epsilons[i]/Delta)] - P_e_min)  / abs( P_e_min );
 		
 		for i in range(len(self.targetErrorRate)):	
 			min_diff=999
-			for j in range(STEPS):
+			for j in range(self.STEPS):
 				if ( abs(err[j]-log10(self.targetErrorRate[i])) < min_diff):
 					min_diff = abs(err[j]-log10(self.targetErrorRate[i]))
 					min_i = j
-			fit += min_i/float(STEPS-1) * self.targetErrorRate_w[i];
-
+			fit += min_i/float(self.STEPS-1) * self.targetErrorRate_w[i];
 		
-		##print err
+		#print fit
+		
 		return fit
+	def printResult(self, parameter_list, eval):
+		self.result_line += 1
+		p_list = parameter_list[0:]
+		normalize(p_list)
+		self.file_result.write(`self.result_line`+'\t'+ `self.times_of_eval`+'\teval\t'+`eval`+'\tpara\t' + ' '.join(toStringList(p_list))+'\n')
+		self.file_result.flush()
 
-lt = LT_exp(sys.argv[1])
-# d = lt.D
-# d = d.split()
-# d =parseFloat(d)
-#print lt.fitness_LT(lt.D)
-nes = ExactNES(lt.fitness_LT, lt.D, minimize=True, maxEvaluations=10000, verbose=True)
-print nes.learn()
+
+# class ResultWriter:
+# 	def __init__(self, filename):
+# 		self.filename = filename
+# 		self.file = open(filename, 'w')
+# 	def printLine(self, line):
+# 		self.file.write(line+'\n')
+# 		self.file.flush()
+# 	def close(self):
+# 		self.file.close()
+
+"""main()"""
+filename = sys.argv[1]
+lt = LT_exp(filename)
+# out = dup(sys.stdout, open('result_%s' % filename, 'w'))
+# sys.stdout = out
+# writer = ResultWriter('dist_%s' % filename)
+nes = ExactNES(lt.fitness_LT, lt.D, minimize=True, maxEvaluations=10000, verbose=True, listener=lt.printResult)
+nes_result = nes.learn()
+final = nes_result[0]
+normalize(final)
+final = toStringList(final)
+print ' '.join(final)
+# dist = open('dist_%s' % filename, 'w')
+# dist.write(' '.join(final) + '\n')
+# dist.close()
 # p = pow(2).power
 # from pybrain.optimization import ExactNES
 # print ExactNES(pow(32.60).power, [0], maxEvaluations = 10000, minimize=True).learn()[0][0]
